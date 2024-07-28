88e6089ec687d79df06f651affff86df888da4f0
amihaylovaa <aggmihaylova@gmail.com>
2024-07-28 16:37:19 +0300

    test

--- NewsApp/.idea/sonarlint/issuestore/index.pb
+++ NewsApp/.idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-b
-2app/src/main/java/com/aggm/newsapp/MainActivity.kt,7\e\7ef3de4504da3d55f8333e1bcc1eb970c21c8c3e
 Y
 )app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
--- NewsApp/app/src/main/java/com/aggm/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/aggm/newsapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.aggm.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
