c41cc21d16b17902f09eab1ea84cbc446ad1ccd6
amihaylovaa <aggmihaylova@gmail.com>
2024-07-31 22:07:39 +0300

    set up navigation controller, set fragment view

--- NewsApp/app/src/main/java/com/aggm/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/aggm/newsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.aggm.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
 class MainActivity : AppCompatActivity() {
+
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
