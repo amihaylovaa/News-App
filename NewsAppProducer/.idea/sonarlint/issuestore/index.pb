7ee422f14d9bb4e60a195bd3d58eea3e21fa5c07
amihaylovaa <aggmihaylova@gmail.com>
2024-08-02 22:41:42 +0300

    add app producer

---
+++ NewsAppProducer/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+C
+NewsAppProducer.iml,e\e\eea1c2884b1d06c660765cfdaced7e34f649cf28
+r
+Bsrc/main/java/aggm/personal/newsapp/producers/NewsAppProducer.java,4\5\453ad04e8b52989e5e57f6a1706688c43313d6f7
---
+++ NewsAppProducer/src/main/java/aggm/personal/newsapp/producers/NewsAppProducer.java
@@ -0,0 +1,11 @@
+package aggm.personal.newsapp.producers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAppProducer {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAppProducer.class);
+    }
+}
