151bc888f1fde11ac361fad432a4b216b5ad1e19
amihaylovaa <aggmihaylova@gmail.com>
2024-08-03 21:50:52 +0300

    fix pom

--- NewsAppProducer/src/main/java/aggm/personal/newsapp/producer/NewsAppProducer.java
+++ NewsAppProducer/src/main/java/aggm/personal/newsapp/producer/NewsAppProducer.java
@@ -1,4 +1,4 @@
-package aggm.personal.newsapp.producers;
+package aggm.personal.newsapp.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
