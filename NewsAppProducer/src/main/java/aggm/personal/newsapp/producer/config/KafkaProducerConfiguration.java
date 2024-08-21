0023738437421220f5921fe77b042472e85a7923
amihaylovaa <aggmihaylova@gmail.com>
2024-08-21 21:41:22 +0300

    fix

--- NewsAppProducer/src/main/java/aggm/personal/newsapp/producer/config/KafkaProducerConfiguration.java
+++ NewsAppProducer/src/main/java/aggm/personal/newsapp/producer/config/KafkaProducerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +19,7 @@ public class KafkaProducerConfiguration {
     private String bootstrapServer;
 
     @Bean
-    public Map<String, Object> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -28,7 +29,7 @@ public class KafkaProducerConfiguration {
     }
 
     @Bean
-    public Map<String, Object> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
